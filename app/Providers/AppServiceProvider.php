b46193da655335673c89093e6b977751e67bb352
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2024-10-08 03:18:52 +0500

    add success and error functions in response using macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Response::macro('success', function($message ,$data = null) {
+            $request = app(\Illuminate\Http\Request::class);
+
+            return response()->json([
+                "success" => true,
+                "message" => $message,
+                "data" => $data,
+                "request" => [
+                    "request_type" => $request->method(),
+                    "payload" => $request->input()
+                ]
+            ]);
+        });
+
+        Response::macro('error', function($message, $code, array $arr = []) {
+            $request = app(\Illuminate\Http\Request::class);
+            $response = [
+                'success' => false,
+                'message' => $message,
+                'request' => [
+                    'request_type' => $request->method(),
+                    'payload' => $request->input(),
+                ],
+            ];
+
+            if (count($arr) > 0) {
+                $response = array_merge($response, $arr);
+            }
+
+            return response()->json($response, $code);
+        });
     }
 }
