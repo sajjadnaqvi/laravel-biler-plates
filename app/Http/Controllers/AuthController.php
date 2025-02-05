fbf592d3cbbcc3a013fafd9ba3b4c9fbcd62a29a
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2025-02-05 23:48:17 +0500

    Add signup route and functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\Repositories\IUserRepository;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+
+    private $userRepository;
     
+    public function __contruct( IUserRepository $userRepository) {
+        $this->userRepository = $userRepository;
+    }
+
+    public function signUp(Request $request) {
+        $data = $request->input();
+
+        try {
+            $this->userRepository->storeValidation($data)->validate();
+            $this->userRepository->store($data);
+
+        } catch (\Exception $e) {
+            return $this->handleException($e);
+        }
+    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,10 +5,27 @@ namespace App\Repositories;
 use App\Contracts\Repositories\IUserRepository;
 use App\Models\User;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserRepository extends BaseRepository implements IUserRepository
 {
     public function __construct(User $user) {
         parent::__construct($user);
     }
+
+    public function store(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        return $this->model->created($data);
+    }
+
+    public function storeValidation(array $data) {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+    }
+
 }
